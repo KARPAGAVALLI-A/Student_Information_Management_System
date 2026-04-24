0f6adecb842c357b9c4867ce3a45ebf2f0692b93
KARPAGAVALLI A <24104072@nec.edu.in>
2026-04-24 18:41:30 +0530

    Delete Student_Information_Management_System/Student_Information_Management_System With JDBC/sims directory

--- Student_Information_Management_System/Student_Information_Management_System With JDBC/sims/src/main/java/com/sims/exception/DuplicateStudentException.java
+++
@@ -1,7 +0,0 @@
-package com.sims.exception;
-
-public class DuplicateStudentException extends RuntimeException {
-    public DuplicateStudentException(int studentId) {
-        super("Student with ID " + studentId + " already exists.");
-    }
-}
--- Student_Information_Management_System/Student_Information_Management_System With JDBC/sims/src/main/java/com/sims/exception/StudentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.sims.exception;
-
-public class StudentNotFoundException extends RuntimeException {
-    public StudentNotFoundException(int studentId) {
-        super("Student with ID " + studentId + " not found.");
-    }
-}
